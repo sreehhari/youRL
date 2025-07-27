d85d6af30cd516f613fc7baf0675db6663c774d1
sreehari <sreehari9300@gmail.com>
2025-07-28 00:47:14 +0530

    jwtUtils for jwt authentication done

--- src/main/java/com/jerrycan/yourl/security/jwt/JwtUtils.java
+++ src/main/java/com/jerrycan/yourl/security/jwt/JwtUtils.java
@@ -1,13 +1,25 @@
 package com.jerrycan.yourl.security.jwt;
 
 import com.jerrycan.yourl.service.UserDetailsImpl;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
 import java.util.stream.Collectors;
 
 public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
     //format for jwt token = Authorization -> Bearer -> token
     public String getJwtFromHeader(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
@@ -26,8 +38,32 @@ public class JwtUtils {
                 .subject(username)
                 .claim("roles",userRole)
                 .issuedAt(new Date())
-                .expiration(new Date(new Date().getTime() + 1000 * 60 * 60 * 48))
-                .signWith()
+                .expiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(key())
                 .compact();
+    }
+    //we use the getpayload then getsubject cuz the username is embedded in the subject in the jwts builder function
+    public String getUserNameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) key())
+                    .build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
